eacac27028c058b504ca2d3e9db43e4acfb16771
Kevin Leung <kevinresol@gmail.com>
2018-10-05 23:35:51 +0800

    Update domain

--- src/lix/site/data/CognitoAuth.hx
+++ src/lix/site/data/CognitoAuth.hx
@@ -8,7 +8,7 @@ abstract CognitoAuth(aws.cognito.CognitoAuth) {
 	public function new() {
 		this = new aws.cognito.CognitoAuth({
 			ClientId: 'fvrf50i7h5od9nr1bq4pefcg3',
-			AppWebDomain: 'lix.auth.us-east-2.amazoncognito.com',
+			AppWebDomain: 'login.lix.pm',
 			RedirectUriSignIn: '$SITE_URL/login',
 			RedirectUriSignOut: SITE_URL,
 			TokenScopesArray: ['openid'],
